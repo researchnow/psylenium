29f23ef49cd7d9aa2fb204c046b4ae626d9c8a33
Michael Partlo <mpartlo@researchnow.com>
2017-08-18 14:50:31 -0500

    Finished setup

---
+++ README.rst
@@ -0,0 +1,4 @@
+UI Automation Core Functionality Library
+========================================
+
+A shared repository containing the basic Selenium page-object-model framework for use in automating UI testing.
---
+++ setup.py
@@ -0,0 +1,78 @@
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+from pip.req import parse_requirements
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+install_reqs = parse_requirements('requirements.txt', session='hack')
+
+setup(
+    name='psylenium',
+    version='1.0.0',
+    description='Core Selenium functionality supporting automated UI testing',
+    long_description=long_description,
+    url='https://github.com/researchnow/pysts',
+    author='Michael Partlo',
+    author_email='mpartlo@researchnow.com',
+
+    # Choose your license
+    license='MIT',
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 5 - Production',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Automated Testers / Developers',
+        'Topic :: Software Development :: Testing Tools',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: MIT License',
+
+        'Programming Language :: Python :: 3.6',
+    ],
+
+    # What does your project relate to?
+    keywords='researchnow test automation',
+
+    # You can just specify the packages manually here if your project is
+    # simple. Or you can use find_packages().
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+
+    # Alternatively, if you want to distribute just a my_module.py, uncomment
+    # this:
+    #   py_modules=["my_module"],
+
+    # List run-time dependencies here.  These will be installed by pip when
+    # your project is installed. For an analysis of "install_requires" vs pip's
+    # requirements files see:
+    # https://packaging.python.org/en/latest/requirements.html
+    install_requires=[str(r.req) for r in install_reqs],
+
+    # List additional groups of dependencies here (e.g. development
+    # dependencies). You can install these using the following syntax,
+    # for example:
+    # $ pip install -e .[dev,test]
+    extras_require={},
+
+    # If there are data files included in your packages that need to be
+    # installed, specify them here.  If using Python 2.6 or less, then these
+    # have to be included in MANIFEST.in as well.
+    package_data={},
+
+    # To provide executable scripts, use entry points in preference to the
+    # "scripts" keyword. Entry points provide cross-platform support and allow
+    # pip to create the appropriate form of executable for the target platform.
+    # entry_points={}
+)

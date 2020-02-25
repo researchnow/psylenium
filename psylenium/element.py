bf7c97b54fe93f95223c6f163a288f1a901ec9ad
Deborah Hwang <dhwang@researchnow.com>
2020-02-25 15:38:01 -0600

    add if else for horizontal or vertical slider

--- psylenium/element.py
+++ psylenium/element.py
@@ -236,8 +236,11 @@ class Element(object):
         hover = ActionChains(self.driver).move_to_element(self.web_element)
         hover.perform()
 
-    def slide(self, percentage):
-        slider = ActionChains(self.driver).click_and_hold(self.web_element).move_by_offset(percentage, 0).release()
+    def drag_and_drop(self, percentage, horizontal=True):
+        if horizontal:
+            slider = ActionChains(self.driver).click_and_hold(self.web_element).move_by_offset(percentage, 0).release()
+        else:
+            slider = ActionChains(self.driver).click_and_hold(self.web_element).move_by_offset(0, percentage).release()
         slider.perform()
 
     def double_click(self):

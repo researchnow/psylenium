de79db41fd167fca14e95605b30d4fcf1d036e14
Deborah Hwang <43755324+debyhwang@users.noreply.github.com>
2020-12-03 15:34:55 -0600

    Revert "Update setup.py"

--- setup.py
+++ setup.py
@@ -1,19 +1,12 @@
-"""A setuptools based setup module.
-See:
-https://packaging.python.org/en/latest/distributing.html
-https://github.com/pypa/sampleproject
-"""
-
 # Always prefer setuptools over distutils
 from setuptools import setup, find_packages
 # To use a consistent encoding
 from codecs import open
 from os import path
-
 try:
-    from pip._internal.req import parse_requirements
-except ImportError:
     from pip.req import parse_requirements
+except ImportError:
+    from pip._internal.req import parse_requirements
 
 here = path.abspath(path.dirname(__file__))
 
@@ -22,26 +15,6 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
 install_reqs = parse_requirements('requirements.txt', session='hack')
-
-
-def handle_http_requirements(install_reqs):
-    requirements = []
-    for _req in install_reqs:
-        try:
-            req = _req.requirement
-        except AttributeError:
-            req = _req.req
-        pkg_str = req
-        if 'http' in req:
-            pkg_name = req.rsplit('#egg=')[-1]
-            pkg_str = f"{pkg_name} @ {req}"
-        requirements.append(pkg_str)
-    return requirements
-
-
-requirements = handle_http_requirements(install_reqs)
-
-print(requirements)
 
 setup(
     name='psylenium',
@@ -57,6 +30,10 @@ setup(
 
     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
         'Development Status :: 5 - Production',
 
         # Indicate who your project is intended for
@@ -66,8 +43,6 @@ setup(
         # Pick your license as you wish (should match "license" above)
         'License :: OSI Approved :: MIT License',
 
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
         'Programming Language :: Python :: 3.6',
     ],
 
@@ -86,7 +61,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=requirements,
+    install_requires=[str(r.req) for r in install_reqs],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
@@ -97,7 +72,7 @@ setup(
     # If there are data files included in your packages that need to be
     # installed, specify them here.  If using Python 2.6 or less, then these
     # have to be included in MANIFEST.in as well.
-    package_data={}
+    package_data={},
 
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow

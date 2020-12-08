dc8cffcc2f9f871d140b2937810255cb96701748
Michael Partlo <michael.partlo@dynata.com>
2020-12-08 10:49:32 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -61,7 +61,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=[str(r.req) for r in install_reqs],
+    install_requires=[str(r.req if 'req' in r.__dict__ else r.requirement) for r in install_reqs],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
